222cdf7a0798c226bf316a60748e8a06f63e33cb
Žanas Stundys <z.stundys@gmail.com>
2024-05-19 14:31:25 +0300

    Setup own photos cataloging

---
+++ src/catalog-photos.js
@@ -0,0 +1,68 @@
+import fs from "fs-extra";
+import path from "path";
+import { groupBy } from "lodash-es";
+
+const SOURCE_DIR = path.join(import.meta.dirname, "./output/keep-raw/images");
+const TARGET_DIR = "D:\\Pictures\\Photos";
+
+/**
+ *  Groups files by date and moves them to the Photos catalog
+ */
+export async function catalogPhotos() {
+    const mediaByFolder = Object.entries(await groupFilesByDateMonth(SOURCE_DIR));
+    const debugMediaByFolder = Object.fromEntries(
+        mediaByFolder.map(([folderPath, files]) => [folderPath, files.map((f) => path.basename(f.path))])
+    );
+    const totalFiles = mediaByFolder.reduce((acc, [, files]) => acc + files.length, 0);
+
+    if (totalFiles === 0) {
+        console.log(`No files found in "${SOURCE_DIR}" folder.`);
+        return;
+    }
+
+    console.log(`Cataloging ${totalFiles} files...`);
+    console.log(JSON.stringify(debugMediaByFolder, null, 2));
+
+    //  Transfer files to target folders
+    for (const [targetFolderPath, files] of mediaByFolder) {
+        await fs.ensureDir(targetFolderPath);
+
+        for (const file of files) {
+            const from = file.path;
+            const to = path.join(targetFolderPath, path.basename(file.path));
+
+            await fs.move(from, to);
+        }
+    }
+
+    console.log(`Files cataloged successfully. Total: ${totalFiles}`);
+}
+
+//  Format date as YYYY-MM using Intl
+const yearMonthFormatter = new Intl.DateTimeFormat("lt-LT", {
+    year: "numeric",
+    month: "2-digit",
+});
+
+/**
+ * Takes in a folder containing a set of images/videos, and generates a tree structure of files
+ * @param {string} folderPath
+ */
+async function groupFilesByDateMonth(folderPath) {
+    const filesWithStats = await fs.readdir(folderPath).then((fileNames) => {
+        const mediaFileNames = fileNames.filter((fileName) => /\.(dng|jpg|mp4)$/i.test(fileName));
+
+        return Promise.all(
+            mediaFileNames.map(async (fileName) => {
+                const filePath = path.join(folderPath, fileName);
+                return { path: filePath, stats: await fs.stat(filePath) };
+            })
+        );
+    });
+
+    const grouped = groupBy(filesWithStats, (file) =>
+        path.join(TARGET_DIR, yearMonthFormatter.format(file.stats.mtime))
+    );
+
+    return grouped;
+}
--- src/keep-raw.js
+++ src/keep-raw.js
@@ -9,8 +9,10 @@
 import fs from "fs-extra";
 import path from "path";
 import { fileURLToPath } from "url";
+
 import { transferAndroidImages, removeAndroidImages } from "./transfer-android-images.js";
 import inquirer from "inquirer";
+import { catalogPhotos } from "./catalog-photos.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -18,105 +20,127 @@ const PHONE_PHOTOS_SOURCE_DIRS = ["/sdcard/DCIM/Camera", "/sdcard/DCIM/OpenCamer
 const IMAGES_DIR = path.join(__dirname, "./output/keep-raw/images");
 const DUPLICATES_DIR = path.join(__dirname, "./output/keep-raw/duplicates");
 
-await transferAndroidImages(PHONE_PHOTOS_SOURCE_DIRS, IMAGES_DIR);
+await run();
 
-const jpegFiles = readJpegFiles();
-const allFiles = readAllFiles();
+async function run() {
+    //  MARK: Transfer files
+    await transferAndroidImages(PHONE_PHOTOS_SOURCE_DIRS, IMAGES_DIR);
 
-console.log(`Found ${jpegFiles.length} JPEG files in "${IMAGES_DIR}" folder...`);
+    const jpegFiles = readJpegFiles();
+    const allFiles = readAllFiles();
 
-const duplicateJpegFiles = filterDuplicates(jpegFiles);
-const trashedFiles = filterTrashed(allFiles);
+    console.log(`Found ${jpegFiles.length} JPEG files in "${IMAGES_DIR}" folder...`);
 
-if (duplicateJpegFiles.length) {
-    console.log(`Moving ${duplicateJpegFiles.length} JPEG files to "${DUPLICATES_DIR}" folder...`);
+    const duplicateJpegFiles = filterDuplicates(jpegFiles);
+    const trashedFiles = filterTrashed(allFiles);
 
-    moveFilesToDuplicatesFolder(duplicateJpegFiles);
+    //  MARK: Duplicate files
+    if (duplicateJpegFiles.length) {
+        console.log(`Moving ${duplicateJpegFiles.length} JPEG files to "${DUPLICATES_DIR}" folder...`);
 
-    /** @type {{ deleteDuplicates: boolean; }} */
-    const answers = await inquirer.prompt([
+        moveFilesToDuplicatesFolder(duplicateJpegFiles);
+
+        /** @type {{ deleteDuplicates: boolean; }} */
+        const answers = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "deleteDuplicates",
+                message: 'Delete all JPEG files from "duplicates" folder?',
+            },
+        ]);
+
+        if (answers.deleteDuplicates) {
+            console.log(`Deleting all JPEG files from "duplicates" folder...`);
+
+            // Delete all JPEG files from "duplicates" folder except .gitignore file
+            const duplicatesFiles = fs.readdirSync(DUPLICATES_DIR).filter((fileName) => fileName !== ".gitignore");
+
+            for (const fileName of duplicatesFiles) {
+                const filePath = path.resolve(DUPLICATES_DIR, fileName);
+                fs.removeSync(filePath);
+            }
+        }
+    } else {
+        console.log(`No JPEG files found that have RAW images.`);
+    }
+
+    //  MARK: Trashed files
+    if (trashedFiles.length) {
+        console.log(`Moving ${trashedFiles.length} JPEG files to "trashed" folder...`);
+
+        for (const trashedFile of trashedFiles) {
+            const src = path.resolve(IMAGES_DIR, trashedFile);
+            const dest = path.resolve(__dirname, "./output/keep-raw/trashed", trashedFile);
+
+            fs.moveSync(src, dest, {
+                overwrite: true,
+            });
+        }
+
+        const { deleteTrashed } = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "deleteTrashed",
+                message: 'Delete all JPEG files from "trashed" folder?',
+                default: true,
+            },
+        ]);
+
+        if (deleteTrashed) {
+            console.log(`Deleting all JPEG files from "trashed" folder...`);
+
+            // Delete all JPEG files from "trashed" folder except .gitignore file
+            const trashedFiles = fs
+                .readdirSync(path.join(__dirname, "./output/keep-raw/trashed"))
+                .filter((fileName) => fileName !== ".gitignore");
+
+            for (const fileName of trashedFiles) {
+                const filePath = path.resolve(__dirname, "./output/keep-raw/trashed", fileName);
+                fs.removeSync(filePath);
+            }
+        }
+    }
+
+    console.log(`Done`);
+
+    //  MARK:  Delete all
+    const { dangerouslyDeleteFromAndroid, dangerouslyDeleteFromAndroidConfirm } = await inquirer.prompt([
         {
             type: "confirm",
-            name: "deleteDuplicates",
-            message: 'Delete all JPEG files from "duplicates" folder?',
+            name: "dangerouslyDeleteFromAndroid",
+            message: "Delete all transferred files from Android phone?",
+            default: false,
+        },
+        {
+            type: "confirm",
+            name: "dangerouslyDeleteFromAndroidConfirm",
+            message: "Are you really sure? Changes cannot be undone.",
+            default: false,
+            when: (answers) => answers.dangerouslyDeleteFromAndroid,
         },
     ]);
 
-    if (answers.deleteDuplicates) {
-        console.log(`Deleting all JPEG files from "duplicates" folder...`);
+    if (dangerouslyDeleteFromAndroid && dangerouslyDeleteFromAndroidConfirm) {
+        console.log(`Deleting all transferred files from Android phone...`);
 
-        // Delete all JPEG files from "duplicates" folder except .gitignore file
-        const duplicatesFiles = fs.readdirSync(DUPLICATES_DIR).filter((fileName) => fileName !== ".gitignore");
+        await removeAndroidImages(PHONE_PHOTOS_SOURCE_DIRS);
 
-        for (const fileName of duplicatesFiles) {
-            const filePath = path.resolve(DUPLICATES_DIR, fileName);
-            fs.removeSync(filePath);
-        }
-    }
-} else {
-    console.log(`No JPEG files found that have RAW images.`);
-}
-
-if (trashedFiles.length) {
-    console.log(`Moving ${trashedFiles.length} JPEG files to "trashed" folder...`);
-
-    for (const trashedFile of trashedFiles) {
-        const src = path.resolve(IMAGES_DIR, trashedFile);
-        const dest = path.resolve(__dirname, "./output/keep-raw/trashed", trashedFile);
-
-        fs.moveSync(src, dest, {
-            overwrite: true,
-        });
+        console.log(`Done`);
     }
 
-    const { deleteTrashed } = await inquirer.prompt([
+    //  MARK:  Delete all
+    const { catalogConfirm } = await inquirer.prompt([
         {
             type: "confirm",
-            name: "deleteTrashed",
-            message: 'Delete all JPEG files from "trashed" folder?',
+            name: "catalogConfirm",
+            message: 'Catalog transfered photos? (Group by date like "2024-01" and move to Photos folder)',
             default: true,
         },
     ]);
 
-    if (deleteTrashed) {
-        console.log(`Deleting all JPEG files from "trashed" folder...`);
-
-        // Delete all JPEG files from "trashed" folder except .gitignore file
-        const trashedFiles = fs
-            .readdirSync(path.join(__dirname, "./output/keep-raw/trashed"))
-            .filter((fileName) => fileName !== ".gitignore");
-
-        for (const fileName of trashedFiles) {
-            const filePath = path.resolve(__dirname, "./output/keep-raw/trashed", fileName);
-            fs.removeSync(filePath);
-        }
+    if (catalogConfirm) {
+        await catalogPhotos();
     }
-}
-
-console.log(`Done`);
-
-const { dangerouslyDeleteFromAndroid, dangerouslyDeleteFromAndroidConfirm } = await inquirer.prompt([
-    {
-        type: "confirm",
-        name: "dangerouslyDeleteFromAndroid",
-        message: "Delete all transferred files from Android phone?",
-        default: false,
-    },
-    {
-        type: "confirm",
-        name: "dangerouslyDeleteFromAndroidConfirm",
-        message: "Are you really sure? Changes cannot be undone.",
-        default: false,
-        when: (answers) => answers.dangerouslyDeleteFromAndroid,
-    },
-]);
-
-if (dangerouslyDeleteFromAndroid && dangerouslyDeleteFromAndroidConfirm) {
-    console.log(`Deleting all transferred files from Android phone...`);
-
-    await removeAndroidImages(PHONE_PHOTOS_SOURCE_DIRS);
-
-    console.log(`Done`);
 }
 
 /**
